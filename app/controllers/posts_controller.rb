a514d26ca9956e63a378e0099ff8a17b23857795
Preston Copeland <copelandps1@gmail.com>
2015-05-19 17:57:19 -0400

    implement post CRUD functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,40 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    flash.notice = "Post '#{@post.title}' created!"
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    flash.notice = "Post '#{@post.title}' updated!"
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash.notice = "Post '#{@post.title}' updated!"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
