3a860849e5af685f9e90d05d00f93a5ca8724403
Preston Copeland <copelandps1@gmail.com>
2015-05-21 12:01:09 -0400

    portfolio index and show views, portfolio link from homepage

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,46 @@
 class PortfoliosController < ApplicationController
 
   def index
+    @portfolios = Portfolio.all
   end
+
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    @portfolio.save
+    flash.notice = "New portfolio project '#{@portfolio.title}' created!"
+    redirect_to portfolio_path(@portfolio)
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.update(portfolio_params)
+    flash.notice = "Portfolio project '#{@portfolio.title}' updated!"
+    redirect_to portfolio_path(@portfolio)
+  end
+
+  def destroy
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.destroy
+    flash.notice = "Portfolio project '#{@portfolio.title}' deleted!"
+    redirect_to portfolios_path
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :description)
+  end
+
 end
