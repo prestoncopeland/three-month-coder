782f945d3b6c9835af60c90bbc25562821ebdd9b
Preston Copeland <copelandps1@gmail.com>
2015-06-05 00:32:07 -0400

    order milestones in index view

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -2,7 +2,7 @@ class MilestonesController < ApplicationController
 before_action :require_login, except: [:index]
 
   def index
-    @milestones = Milestone.all
+    @milestones = Milestone.all.order(:created_at)
     @total_hours = Milestone.all.map do |milestone| milestone.hours
     end.inject(:+)
   end
